baf29b3de94c49054c6d939922982157f7566e11
Nutchyk <williamanandaw@gmail.com>
2023-06-03 17:09:37 +0700

    commit

--- app/src/main/java/com/example/ux_project_axforasset/AktivitasNgasal.java
+++ app/src/main/java/com/example/ux_project_axforasset/AktivitasNgasal.java
@@ -2,9 +2,13 @@ package com.example.ux_project_axforasset;
 
 public class AktivitasNgasal {
 <<<<<<< HEAD
+<<<<<<< HEAD
   // kamu tolol
     // iya
 =======
   // ayam
 >>>>>>> 0964a2c (tes2)
+=======
+  // bebek
+>>>>>>> d468ce0 (iya2)
 }
