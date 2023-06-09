af9bc265fe56297bfbc5312c183e6184e7ed962e
nt <nicholast.theodore@binus.ac.id>
2023-06-10 01:07:59 +0700

    drawer(sidemenu pindah2 belum)

--- app/src/main/java/com/example/ux_project_axforasset/Homepage.java
+++ app/src/main/java/com/example/ux_project_axforasset/Homepage.java
@@ -1,16 +1,21 @@
 package com.example.ux_project_axforasset;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavAction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.android.material.navigation.NavigationView;
 
-public class Homepage extends AppCompatActivity {
+public class Homepage extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     DrawerLayout drawerLayout;
     NavigationView navigationView;
     Toolbar toolbar;
@@ -26,9 +31,50 @@ public class Homepage extends AppCompatActivity {
 
         setSupportActionBar(toolbar);
 
+        Menu menu = navigationView.getMenu();
+        menu.findItem(R.id.home_menu).setVisible(false);
+
+        navigationView.bringToFront();
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
                 R.string.nav_open_drawer, R.string.nav_close_drawer);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+        navigationView.setCheckedItem(R.id.home_menu);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }else{
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+//        switch (item.getItemId()){
+//            case R.id.home_menu:
+//                break;
+//            case R.id.items_menu:
+//                Intent intent = new Intent(Homepage.this, ItemPage.class);
+//                startActivity(intent);
+//                break;
+//            case R.id.profile_menu:
+//                Intent intent = new Intent(Homepage.this, profile.class);
+//                startActivity(intent);
+//                break;
+//            case R.id.logout_menu:
+//                Intent intent = new Intent(Homepage.this, Login.class);
+//                startActivity(intent);
+//                break;
+//        }
+
+        drawerLayout.closeDrawer((GravityCompat.START));
+        return true;
     }
 }
