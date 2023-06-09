e025f690c2fd91c9716abf49bf7c1c3030811aac
nt <nicholast.theodore@binus.ac.id>
2023-06-09 21:38:35 +0700

    drawer(sidemenu kelar)

--- app/src/main/java/com/example/ux_project_axforasset/Homepage.java
+++ app/src/main/java/com/example/ux_project_axforasset/Homepage.java
@@ -1,13 +1,34 @@
 package com.example.ux_project_axforasset;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavAction;
 
 import android.os.Bundle;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class Homepage extends AppCompatActivity {
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.nav_open_drawer, R.string.nav_close_drawer);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
     }
 }
