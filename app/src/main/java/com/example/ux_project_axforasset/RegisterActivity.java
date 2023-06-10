3f587bd58524cb581e81bdec0778f2a90fb6fb5a
win <artluciel@gmail.com>
2023-06-10 09:29:50 +0800

    tinggal restriction home sama profile

---
+++ app/src/main/java/com/example/ux_project_axforasset/RegisterActivity.java
@@ -0,0 +1,38 @@
+package com.example.ux_project_axforasset;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+
+
+    Button btn1 = findViewById(R.id.regisregis);
+        btn1.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(RegisterActivity.this, loginActivity.class);
+            startActivity(intent);
+        }
+    });
+
+    Button btn2 = findViewById(R.id.regislogin);
+        btn2.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(RegisterActivity.this, loginActivity.class);
+            startActivity(intent);
+        }
+    });
+
+}
+}
