b0c9d96979fea20dbcbfba4b7bccb344b19a121c
win <artluciel@gmail.com>
2023-06-10 17:08:26 +0800

    otw kerja profile login lom global variable

--- app/src/main/java/com/example/ux_project_axforasset/Login.java
+++ app/src/main/java/com/example/ux_project_axforasset/Login.java
@@ -10,36 +10,26 @@ import android.widget.EditText;
 
 public class Login extends AppCompatActivity {
 
-    Button buttonLogin = findViewById(R.id.llogin);
-    EditText ltfname = findViewById(R.id.ltfname);
-    EditText ltfpw = findViewById(R.id.ltfpw);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        Button buttonLogin = findViewById(R.id.llogin);
+        Button btn2 = findViewById(R.id.regisb);
+        EditText ltfname = findViewById(R.id.ltfname);
+        EditText ltfpw = findViewById(R.id.ltfpw);
 
-        Button btn1 = findViewById(R.id.llogin);
-        btn1.setOnClickListener(new View.OnClickListener() {
+
+        btn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Login.this, Homepage.class);
+                Intent intent = new Intent(Login.this, RegisterActivity.class);
                 startActivity(intent);
             }
         });
 
-//        Button btn2 = findViewById(R.id.lregis);
-//        btn2.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(loginActivity.this, RegisterActivity.class);
-//                startActivity(intent);
-//            }
-//        });
-//
-    }
-
-    private boolean validateUsername() {
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -50,7 +40,7 @@ public class Login extends AppCompatActivity {
                     ltfname.setError("Username tidak boleh kosong");
                 } else if (password.isEmpty()) {
                     ltfpw.setError("Password tidak boleh kosong");
-                } else if (password.length()<8) {
+                } else if (password.length() < 8) {
                     ltfpw.setError("Password minimal memiliki 8 karakter");
                 } else {
                     Intent intent = new Intent(Login.this, Homepage.class);
@@ -58,9 +48,5 @@ public class Login extends AppCompatActivity {
                 }
             }
         });
-        return true;
     }
-
-
-
 }
--- app/src/main/java/com/example/ux_project_axforasset/RegisterActivity.java
+++ app/src/main/java/com/example/ux_project_axforasset/RegisterActivity.java
@@ -2,39 +2,37 @@ package com.example.ux_project_axforasset;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-
-public class MainActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register);
 
-        Button btn1 = findViewById(R.id.login_btn);
+
+
+        Button btn1 = findViewById(R.id.regisregis);
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Login.class);
+                Intent intent = new Intent(RegisterActivity.this, Login.class);
                 startActivity(intent);
             }
         });
 
-        Button btn2 = findViewById(R.id.register_btn);
+        Button btn2 = findViewById(R.id.regislogin);
         btn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                Intent intent = new Intent(RegisterActivity.this, Login.class);
                 startActivity(intent);
             }
         });
 
     }
-
 }
