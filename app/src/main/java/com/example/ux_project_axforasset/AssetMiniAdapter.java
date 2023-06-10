bfc2eaa3e6edc04bb48fb5fe5d35c47d24ab0967
Nutchyk <williamanandaw@gmail.com>
2023-06-10 21:10:25 +0700

    Commitan

---
+++ app/src/main/java/com/example/ux_project_axforasset/AssetMiniAdapter.java
@@ -0,0 +1,73 @@
+package com.example.ux_project_axforasset;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.Vector;
+
+public class AssetMiniAdapter extends RecyclerView.Adapter<AssetMiniAdapter.ViewHolder>{
+
+    Vector<AssetListItem> assetListItem;
+    Context context;
+    ClickInterface clickInterface;
+
+    public AssetMiniAdapter(Context context, ClickInterface clickInterface) {
+        this.context = context;
+        this.clickInterface = clickInterface;
+    }
+
+    public void setAssetListItem(Vector<AssetListItem> assetListItem) {
+        this.assetListItem = assetListItem;
+    }
+
+
+    @NonNull
+    @Override
+    public AssetMiniAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(context).inflate(R.layout.rv_smallassetlist, parent, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AssetMiniAdapter.ViewHolder holder, int position) {
+        holder.tv_AssetName.setText(assetListItem.get(position).getAssetName());
+        holder.tv_AssetShortDesc.setText(assetListItem.get(position).getAssetShortDesc());
+        holder.iv_AssetImage.setImageResource(assetListItem.get(position).getAssetImage());
+
+        holder.itemView.findViewById(R.id.assetItem).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clickInterface.onClick(holder.getBindingAdapterPosition());
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return assetListItem.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        TextView tv_AssetName, tv_AssetShortDesc;
+        ImageView iv_AssetImage;
+
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            tv_AssetName = itemView.findViewById(R.id.sml_assetName);
+            tv_AssetShortDesc = itemView.findViewById(R.id.sml_assetDesc);
+            iv_AssetImage = itemView.findViewById(R.id.sml_imageView);
+        }
+
+    }
+}
--- app/src/main/java/com/example/ux_project_axforasset/Homepage.java
+++ app/src/main/java/com/example/ux_project_axforasset/Homepage.java
@@ -4,13 +4,15 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.MenuItem;
 
 import com.google.android.material.navigation.NavigationView;
@@ -18,7 +20,7 @@ import com.google.android.material.navigation.NavigationView;
 import java.util.TimerTask;
 import java.util.Vector;
 
-public class Homepage extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class Homepage extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, ClickInterface {
     DrawerLayout drawerLayout;
     NavigationView navigationView;
     Toolbar toolbar;
@@ -26,29 +28,35 @@ public class Homepage extends AppCompatActivity implements NavigationView.OnNavi
     private Vector<Integer> listImage;
     private ViewPager viewPager;
 
+    ConstraintLayout assetItem1;
+
+    // semua recyclerview
+    RecyclerView rv_peringkatAtas, rv_palingpopuler;
+    Vector<AssetListItem> assetlistitem1, assetlistitem2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
 
-        drawerLayout = findViewById(R.id.drawer_layout);
-        navigationView = findViewById(R.id.nav_view);
-        toolbar = findViewById(R.id.toolbar);
+        drawerLayout = findViewById(R.id.rv_palingpopuler);
+//        navigationView = findViewById(R.id.nav_view);
+//        toolbar = findViewById(R.id.toolbar);
 
         setSupportActionBar(toolbar);
 
-        Menu menu = navigationView.getMenu();
-        menu.findItem(R.id.home_menu).setVisible(false);
+//        Menu menu = navigationView.getMenu();
+//        menu.findItem(R.id.home_menu).setVisible(false);
 
-        navigationView.bringToFront();
+//        navigationView.bringToFront();
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
                 R.string.nav_open_drawer, R.string.nav_close_drawer);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
 
-        navigationView.setNavigationItemSelectedListener(this);
+//        navigationView.setNavigationItemSelectedListener(this);
 
-        navigationView.setCheckedItem(R.id.home_menu);
+//        navigationView.setCheckedItem(R.id.home_menu);
 
 
 
@@ -63,6 +71,13 @@ public class Homepage extends AppCompatActivity implements NavigationView.OnNavi
 
         java.util.Timer timer = new java.util.Timer();
         timer.scheduleAtFixedRate(new The_slide_timer(),2000,3000);
+
+        getSemuaItem();
+    }
+
+    @Override
+    public void onClick(int position) {
+
     }
 
     public class The_slide_timer extends TimerTask {
@@ -108,4 +123,71 @@ public class Homepage extends AppCompatActivity implements NavigationView.OnNavi
         drawerLayout.closeDrawer((GravityCompat.START));
         return true;
     }
+
+    public void getSemuaItem () {
+        // Get semua assetItem. maaf gapake for loop
+        rv_peringkatAtas = (RecyclerView) findViewById(R.id.rv_peringkatatas);
+        rv_palingpopuler = (RecyclerView) findViewById(R.id.rv_paling_populer);
+
+        assetlistitem1 = new Vector<>();
+        assetlistitem2 = new Vector<>();
+
+        assetlistitem1.add(new AssetListItem(
+                "Wizard duck",
+                "A Toon Styled 3D Asset",
+                "$12.99",
+                R.drawable.wizardduck,
+                "Duck who mastered the art of magic. " +
+                        "This asset comes with 2 different " +
+                        "texture variants and sample animations"));
+
+        assetlistitem1.add(new AssetListItem(
+                "Angry Man",
+                "A Cartoon Styled 3D Model",
+                "$9.99",
+                R.drawable.angryman,
+                "An Angry man 3D Asset. Beware of this man. " +
+                        "An asset with single texture variant. " +
+                        "Fully Rigged."));
+
+        assetlistitem1.add(new AssetListItem(
+                "Lotus Evora S",
+                "A Realistic Styled 3D Asset",
+                "$15.99",
+                R.drawable.evoras,
+                "A 3D asset. Sportscar model. Comes with " +
+                        "sample animations and fully rigged."));
+
+        assetlistitem2.add(new AssetListItem(
+                "Wizard duck",
+                "A Toon Styled 3D Asset",
+                "$12.99",
+                R.drawable.wizardduck,
+                "Duck who mastered the art of magic. " +
+                        "This asset comes with 2 different " +
+                        "texture variants and sample animations"));
+
+        assetlistitem2.add(new AssetListItem(
+                "Angry Man",
+                "A Cartoon Styled 3D Model",
+                "$9.99",
+                R.drawable.angryman,
+                "An Angry man 3D Asset. Beware of this man. " +
+                        "An asset with single texture variant. " +
+                        "Fully Rigged."));
+
+        AssetMiniAdapter adapter1 = new AssetMiniAdapter(this, this);
+        adapter1.setAssetListItem(assetlistitem1);
+
+        AssetMiniAdapter adapter2 = new AssetMiniAdapter(this, this);
+        adapter2.setAssetListItem(assetlistitem2);
+
+
+        rv_peringkatAtas.setAdapter(adapter1);
+        rv_peringkatAtas.setLayoutManager(new GridLayoutManager(this, 3));
+        rv_palingpopuler.setAdapter(adapter2);
+        rv_palingpopuler.setLayoutManager(new GridLayoutManager(this, 2));
+
+    }
+
 }
